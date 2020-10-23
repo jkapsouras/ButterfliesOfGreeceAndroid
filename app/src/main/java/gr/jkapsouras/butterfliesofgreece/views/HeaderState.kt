71d25d99bb1be18a637dc80fa86837be694532d3
Ioannis Kapsouras <jkapsouras@san-soft.com>
2020-10-24 00:02:54 +0300

    add header state

--- app/src/main/java/gr/jkapsouras/butterfliesofgreece/views/HeaderState.kt
+++ app/src/main/java/gr/jkapsouras/butterfliesofgreece/views/HeaderState.kt
@@ -1,5 +1,23 @@
 package gr.jkapsouras.butterfliesofgreece.views
 
-class HeaderState{
+import gr.jkapsouras.butterfliesofgreece.dto.ButterflyPhoto
+import gr.jkapsouras.butterfliesofgreece.extensions.uniques
+import gr.jkapsouras.butterfliesofgreece.families.ViewArrange
 
+class HeaderState(val photosToPrint:List<ButterflyPhoto>?,
+                  val currentArrange: ViewArrange,
+                  val headerName:String){
+}
+
+fun HeaderState.with(currentArrange:ViewArrange? = null, photosToPrint:List<ButterflyPhoto>? = null, headerName:String? = null): HeaderState{
+    if(photosToPrint == null){
+        return HeaderState(this.photosToPrint, currentArrange ?: this.currentArrange, headerName ?: this.headerName)
+    }
+
+    if(this.photosToPrint == null){
+        return HeaderState(photosToPrint, currentArrange ?: this.currentArrange, headerName ?: this.headerName )
+    }
+
+    val tmpPhotos = this.photosToPrint + photosToPrint
+    return HeaderState(tmpPhotos.uniques(), currentArrange ?: this.currentArrange, headerName ?: this.headerName)
 }
