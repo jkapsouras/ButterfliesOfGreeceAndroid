1cd8f582315d128264f6ba5a89ccd7eb32629d3a
Yannis Kapsouras <jkapsouras@gmail.com>
2020-10-23 12:30:06 +0300

    add dtos and data.json parsing

--- app/src/main/java/gr/jkapsouras/butterfliesofgreece/base/di/dependencyModule.kt
+++ app/src/main/java/gr/jkapsouras/butterfliesofgreece/base/di/dependencyModule.kt
@@ -5,21 +5,28 @@ import gr.jkapsouras.butterfliesofgreece.base.schedulers.BackgroundThreadSchedul
 import gr.jkapsouras.butterfliesofgreece.base.schedulers.IBackgroundThread
 import gr.jkapsouras.butterfliesofgreece.base.schedulers.IMainThread
 import gr.jkapsouras.butterfliesofgreece.base.schedulers.MainThreadScheduler
+import gr.jkapsouras.butterfliesofgreece.data.Storage
 import gr.jkapsouras.butterfliesofgreece.families.FamiliesPresenter
 import gr.jkapsouras.butterfliesofgreece.main.MenuPresenter
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val butterfliesModule = module {
     registerSchedulers(this)
+    registerDataSources(this)
     registerPresenters(this)
 }
 
 fun registerSchedulers(module: Module){
     module.single<IBackgroundThread> { BackgroundThreadScheduler(Schedulers.newThread()) }
     module.single<IMainThread> { MainThreadScheduler(AndroidSchedulers.mainThread())}
+}
+
+fun registerDataSources(module: Module){
+    module.single { Storage(androidContext()) }
 }
 
 fun registerPresenters(module: Module){
--- app/src/main/java/gr/jkapsouras/butterfliesofgreece/main/MainMenuFragment.kt
+++ app/src/main/java/gr/jkapsouras/butterfliesofgreece/main/MainMenuFragment.kt
@@ -11,6 +11,7 @@ import gr.jkapsouras.butterfliesofgreece.MainActivity
 import gr.jkapsouras.butterfliesofgreece.R
 import gr.jkapsouras.butterfliesofgreece.base.BaseFragment
 import gr.jkapsouras.butterfliesofgreece.base.UiComponent
+import gr.jkapsouras.butterfliesofgreece.data.Storage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_menu_fragment.*
 import org.koin.android.ext.android.inject
@@ -21,6 +22,7 @@ class MainMenuFragment : BaseFragment<MenuPresenter>() {
     override val layoutResource: Int
         get() = R.layout.main_menu_fragment
     override val presenter: MenuPresenter by inject()
+    val storage : Storage by inject()
     var menuComponent:MenuComponent? = null
 
     companion object {
@@ -42,6 +44,7 @@ class MainMenuFragment : BaseFragment<MenuPresenter>() {
     override fun onStart() {
         super.onStart()
         (requireActivity() as AppCompatActivity).toolbar.visibility = View.GONE
+        storage.print()
     }
 
     override fun onResume() {
